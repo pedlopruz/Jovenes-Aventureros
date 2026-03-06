2cc81c29fcc18ef644e331e899db0b07bbe8a195
jovenes-aventureros <pedlopruz2002@gmail.com>
2026-03-06 13:45:14 +0100

    Cambio url

--- Jovenes_Aventureros/Inicio/urls.py
+++ Jovenes_Aventureros/Inicio/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", home, name="Home"),
+    path("socios/mostrarSocios/", home, name="Home"),
 ]
 
 if settings.DEBUG:
